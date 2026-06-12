e35034e3bff65b5f5d98f7be339d6cce1c94c3c2
Anton Konyahin <me@konyahin.xyz>
2026-06-12 20:47:13 +0300

    feat(api): add API for note archiving

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -8,9 +8,10 @@ import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.getOrFail
 import xyz.konyahin.model.CreateNoteDto
-import xyz.konyahin.model.NoteDto
 import xyz.konyahin.model.Notes
+import java.util.UUID
 
 fun Application.configureRouting() {
     install(ContentNegotiation) {
@@ -20,8 +21,9 @@ fun Application.configureRouting() {
         route("/api/notes") {
             get {
                 val query = call.request.queryParameters["q"]?.takeIf { it.isNotBlank() }
+                val archived = call.request.queryParameters["archived"]?.toBoolean() ?: false
                 call.respond(
-                    Notes.getNotes(query)
+                    Notes.getNotes(query, archived)
                 )
             }
 
@@ -29,6 +31,16 @@ fun Application.configureRouting() {
                 val dto = call.receive<CreateNoteDto>()
                 val createdNote = Notes.createNote(dto)
                 call.respond(HttpStatusCode.Created, createdNote)
+            }
+
+            post("/{id}/archive") {
+                val id = try {
+                    UUID.fromString(call.parameters.getOrFail("id"))
+                } catch (e: IllegalArgumentException) {
+                    return@post call.respond(HttpStatusCode.BadRequest, "invalid uuid")
+                }
+                Notes.archiveNote(id)
+                call.respond(HttpStatusCode.NoContent)
             }
         }
 
--- src/main/kotlin/model/Notes.kt
+++ src/main/kotlin/model/Notes.kt
@@ -1,16 +1,16 @@
 package xyz.konyahin.model
 
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import org.jetbrains.exposed.v1.core.StdOutSqlLogger
+import org.jetbrains.exposed.v1.core.and
 import org.jetbrains.exposed.v1.core.dao.id.java.UUIDTable
+import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.core.like
 import org.jetbrains.exposed.v1.core.lowerCase
 import org.jetbrains.exposed.v1.datetime.timestamp
-import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.insertAndGetId
 import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.jetbrains.exposed.v1.jdbc.update
 import xyz.konyahin.utils.UUIDSerializer
 import java.util.UUID
 import kotlin.time.Clock
@@ -30,11 +30,6 @@ data class CreateNoteDto(
 )
 
 @Serializable
-data class SearchNoteDto(
-    val search: String,
-)
-
-@Serializable
 data class CreatedNoteDto(
     @Serializable(with = UUIDSerializer::class)
     val id: UUID,
@@ -43,35 +38,46 @@ data class CreatedNoteDto(
 
 object Notes : UUIDTable("notes") {
     val content = text("content")
+
     // because SQLite can't make case insensitive search
     val contentLower = text("content_lower")
     val created = timestamp("created_at")
+    val isArchived = bool("is_archived")
 
     fun createNote(dto: CreateNoteDto): CreatedNoteDto = transaction {
         val created = Clock.System.now()
         val id = Notes.insertAndGetId {
-            it[content] = dto.content
-            it[contentLower] = dto.content.lowercase()
+            it[Notes.content] = dto.content
+            it[Notes.contentLower] = dto.content.lowercase()
             it[Notes.created] = created
+            it[Notes.isArchived] = false
         }.value
         CreatedNoteDto(id, created)
     }
 
-    fun getNotes(query: String?) = transaction {
+    fun getNotes(query: String?, archived: Boolean) = transaction {
         val notes = if (query == null) {
-            Notes.selectAll()
+            Notes.selectAll().where {
+                Notes.isArchived eq archived
+            }
         } else {
             Notes.selectAll().where {
-                Notes.contentLower like "%${query.lowercase()}%"
+                (Notes.isArchived eq archived) and (Notes.contentLower like "%${query.lowercase()}%")
             }
         }
 
         notes.map {
             NoteDto(
                 it[Notes.id].value,
-                it[content],
-                it[created],
+                it[Notes.content],
+                it[Notes.created],
             )
+        }
+    }
+
+    fun archiveNote(id: UUID) = transaction {
+        Notes.update({ Notes.id eq id }) {
+            it[Notes.isArchived] = true
         }
     }
 }
